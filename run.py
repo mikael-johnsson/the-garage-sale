c42b31c5516938fc7cc371c4e3fc6920b4decae8
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-04-02 11:43:35 +0000

    connect APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,1 +1,18 @@
+import gspread
+from google.oauth2.service_account import Credentials
+# python3 run.py
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
 
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("the-trade-game")
+
+user = SHEET.worksheet("user")
+data = user.get_all_values()
+print(data)
+     
