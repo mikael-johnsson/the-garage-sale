f70098151dabe9ce47ddba204297ad1d184e9f4e
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-04-02 12:00:50 +0000

    add welcome functions

--- run.py
+++ run.py
@@ -12,7 +12,14 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("the-trade-game")
 
-user = SHEET.worksheet("user")
-data = user.get_all_values()
-print(data)
-     
+def display_instructions():
+    print("WELCOME TO THE TRADE GAME\n")
+    print("The game is simple: trade your thumbtack with higher valued items at one of the five vendors. \nIn ten rounds, your aim is to trade your inventory up as high as you can!")
+    print("Your score is the amount of cash you receive when selling your items when leaving the market.")
+    input_username()
+
+def input_username():
+    username = Input(Enter username here:)
+    return username
+    
+display_instructions()
