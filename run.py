47e210171ac242689e2546960163f8c86a072dcd
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-04-03 07:16:33 +0000

    remove google sheet connection

--- run.py
+++ run.py
@@ -1,17 +1,3 @@
-import gspread
-from google.oauth2.service_account import Credentials
-# python3 run.py
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-
-CREDS = Credentials.from_service_account_file("creds.json")
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open("the-trade-game")
-
 def display_instructions():
     print("WELCOME TO THE TRADE GAME\n")
     print("The game is simple: trade your thumbtack with higher valued items at one of the five vendors. \nIn ten rounds, your aim is to trade your inventory up as high as you can!")
