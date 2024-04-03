eafbe7cba1a5e1ee74867a4fb79646d244bf8b72
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-04-03 07:22:35 +0000

    add data.py file + user and vendor ron

---
+++ data.py
@@ -0,0 +1,14 @@
+user = [
+    {"item": "thumbtack",
+    "value": 1,
+    "quantity": 1,
+    }]
+
+ron = [
+    {"item": "axe",
+    "value": 1
+    "quantity": 1},
+    {"item": "mustache trimmer",
+    "value": 3
+    "quantity": 2}
+]
