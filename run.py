34a5fb960df680fc7aa3fbe1174d1a8fa8039925
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-04-03 07:58:13 +0000

    add docstrings to existing functions

--- run.py
+++ run.py
@@ -1,16 +1,25 @@
 # python3 run.py
 
 def display_instructions():
+    """
+    Displays title and instructions to the game
+    """
     print("WELCOME TO THE TRADE GAME\n")
     print("The game is simple: trade your thumbtack with higher valued items at one of the five vendors. \nIn ten rounds, your aim is to trade your inventory up as high as you can!")
     print("Your score is the amount of cash you receive when selling your items when leaving the market.")
     
 
 def input_username():
+    """
+    Lets the user insert a username
+    """
     username = input("Enter username here:")
     return username
     
 def game_menu():
+    """
+    Displays a menu to allow the user to choose what to do in the game
+    """
     print("1: Trade with Ron\n"
         "2: Show your inventory\n" 
         "3: Show score and exit game")
@@ -33,6 +42,9 @@ def game_menu():
     return choice
 
 def main():
+    """
+    Runs the game
+    """
     display_instructions()
     username = input_username()
     print(game_menu())
