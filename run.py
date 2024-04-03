6b3448cfe543d2759643345c7de35bd86c3de499
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-04-03 07:26:07 +0000

    create main function

--- run.py
+++ run.py
@@ -2,10 +2,14 @@ def display_instructions():
     print("WELCOME TO THE TRADE GAME\n")
     print("The game is simple: trade your thumbtack with higher valued items at one of the five vendors. \nIn ten rounds, your aim is to trade your inventory up as high as you can!")
     print("Your score is the amount of cash you receive when selling your items when leaving the market.")
-    input_username()
+    
 
 def input_username():
     username = Input(Enter username here:)
     return username
     
-display_instructions()
+def main():
+    display_instructions()
+    username = input_username()
+
+main()
