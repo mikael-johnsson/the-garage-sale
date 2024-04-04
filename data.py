4b583bea8a994e0399614e228e6bd3928aeae215
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-04-04 08:25:23 +0000

    add sale modifier key to vendor data

--- data.py
+++ data.py
@@ -4,10 +4,12 @@ user = [
     "quantity": 1
     }]
 
-ron = {"welcome message": "Hi and welcome",
+ron = {"welcome message": "Hello. This is my table of stuff. Trade something if you want, I don't care.",
+        "sale modifer" : 1,
         "items": [{"item": "axe",
                 "value": 1,
                 "quantity": 1},
                 {"item": "mustache trimmer",
                 "value": 3,
-                "quantity": 2}] }
+                "quantity": 2}] 
+    }
