0f9c40b19956adf001a44cf1ba92b3328016861e
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-04-04 13:05:58 +0000

    remove unused user array

--- data.py
+++ data.py
@@ -1,9 +1,3 @@
-"""user = {"luck modifier": 1.2,
-        "items" : {"item": "thumbtack",
-                    "value": 1,
-                    "quantity": 1}
-    }"""
-
 ron = {"welcome message": "Hello. This is my table of stuff. Trade something if you want, I don't care.",
         "sale modifier" : 1,
         "items": [{"item": "axe",
