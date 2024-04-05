94100fd1c895037b1ab74dc291f606e19bb9b5d9
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-04-05 06:57:44 +0000

    add name attribute to vendor class and objects

--- data.py
+++ data.py
@@ -26,20 +26,21 @@ class Vendor:
     """
     Creates vendor
     """
-    def __init__(self, welcome_message, sale_modifier, items):
+    def __init__(self, name, welcome_message, sale_modifier, items):
+        self.name = name
         self.welcome = welcome_message
         self.sale = sale_modifier
         self.items = items
 
 #Vendor objects
-jim = Vendor("Jim: Hi there, want to trade something? Take whatever you want!", 0.7, 
+jim = Vendor("Jim", "Jim: Hi there, want to trade something? Take whatever you want!", 0.7, 
             [{"item": "stapler", "value": 1.2,"quantity": 1},
             {"item": "teapot", "value": 4, "quantity": 1},
             {"item": "magic beans", "value": 6, "quantity": 1},
             {"item": "telescope","value": 12, "quantity": 1},
             {"item": "keyboard", "value": 7, "quantity": 3}])
 
-michael = Vendor(f"Michael: Welcome to my table {player.username}! My items are big - THAT'S WHAT SHE SAID!", 0.6, 
+michael = Vendor("Michael", f"Michael: Welcome to my table {player.username}! My items are big - THAT'S WHAT SHE SAID!", 0.6, 
                 [{"item": "coffee mug","value": 0.8,"quantity": 1},
                 {"item": "reem of paper", "value": 0.5,"quantity": 10},
                 {"item": "an award","value": 2,"quantity": 3},
@@ -47,7 +48,7 @@ michael = Vendor(f"Michael: Welcome to my table {player.username}! My items are 
                 {"item": "plasma tv","value": 9,"quantity": 1}
                 ])
 
-kevin = Vendor("Kevin: Hi... I'm Kevin. This is items. We trade.", 0.7, 
+kevin = Vendor("Kevin", "Kevin: Hi... I'm Kevin. This is items. We trade.", 0.7, 
                 [{"item": "pot of chili","value": 6,"quantity": 1},
                 {"item": "drum kit","value": 13,"quantity": 1},
                 {"item": "cookie","value": 0.8,"quantity":10},
@@ -55,7 +56,7 @@ kevin = Vendor("Kevin: Hi... I'm Kevin. This is items. We trade.", 0.7,
                 {"item": "ice cream cone","value": 1,"quantity": 4}
                 ])
 
-angela = Vendor("Angela: Good afternoon. Before you I lay several quite valuble objects.", 1, 
+angela = Vendor("Angela", "Angela: Good afternoon. Before you I lay several quite valuble objects.", 1, 
                 [{"item": "wood cross","value": 5,"quantity": 1},
                 {"item": "baby jesus statue","value": 7,"quantity": 1},
                 {"item": "open toe shoes","value": 3,"quantity": 3},
@@ -63,7 +64,7 @@ angela = Vendor("Angela: Good afternoon. Before you I lay several quite valuble 
                 {"item": "used cat toy","value": 1,"quantity": 4}
                 ])
 
-phyllis = Vendor("Phyllis: Oh, hello! Please, come take a look. Have you met my husband, Bob Vance, Vance Refrigiration?", 0.8,
+phyllis = Vendor("Phyllis", "Phyllis: Oh, hello! Please, come take a look. Have you met my husband, Bob Vance, Vance Refrigiration?", 0.8,
                 [{"item": "used mini fridge","value": 11,"quantity": 1},
                 {"item": "oven mitt","value": 4,"quantity": 4},
                 {"item": "santa costume","value": 10,"quantity": 1},
