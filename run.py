e538377b44360e2a06a3fdff88c737f027a63797
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-04-05 07:35:15 +0000

    install and import colorama, add color to text

--- run.py
+++ run.py
@@ -1,15 +1,18 @@
 # python3 run.py
 from data import *
 from tabulate import tabulate
+from colorama import Fore, Back, Style
 
 def display_instructions():
     """
     Displays title and instructions to the game
     """
-    print("WELCOME TO THE GARAGE SALE\n")
+    print(Fore.GREEN + "WELCOME TO THE GARAGE SALE") 
+    print(Style.RESET_ALL)
     print("The game is simple: trade your thumbtack with higher valued items at one of the five vendors.\n"
     "Your score is the amount of cash your inventory is worth when leaving the market.\n"
     "Remember: the trade deal needs to be good for both you and the vendor - or they will deny the deal.\n")
+    
     
     
 def input_username():
@@ -17,7 +20,8 @@ def input_username():
     Lets the user insert a username
     """
     player.username = input("Enter username here: ")
-    print("\nIt is time to start the game. Let the great experiment begin!\n")
+    print(Fore.GREEN + "\nIt is time to start the game. Let the great experiment begin!")
+    print(Style.RESET_ALL)
     
 def game_menu():
     """
@@ -53,8 +57,9 @@ def go_to_market():
     choice = ""
     choice_not_made = True
     while(choice_not_made):
-        print("\nWelcome to the garage sale! Choose a table to visit:\n"
-        "1: Jim\n"
+        print(Fore.GREEN + "\nWelcome to the garage sale! Choose a table to visit.")
+        print(Style.RESET_ALL)
+        print("1: Jim\n"
         "2: Michael\n" 
         "3: Angela\n"
         "4: Kevin\n"
@@ -85,7 +90,8 @@ def visit_vendor(vendor):
     Displays the chosen vendors inventory
     """
     
-    print(f"\n{vendor.welcome} \n")
+    print(Fore.GREEN + f"\n{vendor.welcome}")
+    print(Style.RESET_ALL)
     items_list = vendor.items
     for item in items_list:
         print(f"{(items_list.index(item)+1)}: {item["item"].capitalize()}, {item["quantity"]} pcs")
@@ -107,7 +113,8 @@ def visit_vendor(vendor):
                     item_input = False
                     i = chosen_number - 1
                     chosen_item = items_list[i]
-                    print(f"\nSure thing, I have this amount of {items_list[i]["item"]}: {items_list[i]["quantity"]}\n")
+                    print(Fore.GREEN + f"\nSure thing, I have this amount of {items_list[i]["item"]}: {items_list[i]["quantity"]}")
+                    print(Style.RESET_ALL)
                 except ValueError:
                     print(f"Something went wrong, select a number between 1-{len(items_list)}")
 
@@ -116,14 +123,17 @@ def visit_vendor(vendor):
                 try:
                     chosen_qnt = int(input("How many do you want? ")) 
                     if chosen_qnt == 0:
-                            print("You cannot trade for zero of my item.")
+                            print(Fore.RED + "You cannot trade for zero of my item.")
+                            print(Style.RESET_ALL)
                     elif chosen_qnt <= items_list[i]["quantity"]:
                         trade(chosen_item, chosen_qnt, vendor)
                     else: 
-                        print("I don't have that many")
+                        print(Fore.BLUE + "I don't have that many")
+                        print(Style.RESET_ALL)
                         game_menu()
                 except ValueError:
-                    print(f"Something went wrong, select a number")
+                    print(Fore.RED + f"Something went wrong, select a number")
+                    print(Style.RESET_ALL)
 
         else:
             print("Please answer Y or N")
@@ -138,15 +148,17 @@ def trade(item, quantity, vendor):
     if player_value >= vendor_value:
         new_user_item = item
         new_vendor_item = player.items
-        print(f"\n{vendor.name}: I will accept that deal. Here you go.\n"
-        "-- Trade went through, inventory updated --\n")
+        print(Fore.GREEN + f"\n{vendor.name}: I will accept that deal. Here you go.")
+        print(Style.RESET_ALL)
+        print("-- Trade went through, inventory updated --\n")
         player.items = new_user_item
         player.items["quantity"] = quantity
         vendor.items.append(new_vendor_item)
         vendor.items.remove(item)   
     else:
-        print(f"\n{vendor.name}: That's not a good trade for me.\n"
-        "-- Trade failed --\n")
+        print(Fore.BLUE + f"\n{vendor.name}: That's not a good trade for me.")
+        print(Style.RESET_ALL)
+        print("-- Trade failed --\n")
     game_menu()
     
 
