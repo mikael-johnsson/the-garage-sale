1f6633613e9eb3506906730adb12224f5127cd30
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-04-08 09:00:47 +0000

    remove quantity factor from user and vendor objects

--- data.py
+++ data.py
@@ -10,7 +10,7 @@ class User:
     def inventory(self):
         print(self.items["item"].capitalize())
 
-player = User(1.2, {"item": "thumbtack", "value": 1, "quantity": 1}, "")
+player = User(1.2, {"item": "thumbtack", "value": 1}, "")
 
 class Vendor:
     """
@@ -24,42 +24,42 @@ class Vendor:
 
 #Vendor objects
 jim = Vendor("Jim", "Jim: Hi there, want to trade something? Take whatever you want!", 0.7, 
-            [{"item": "stapler", "value": 1.2,"quantity": 1},
-            {"item": "teapot", "value": 4, "quantity": 1},
-            {"item": "magic beans", "value": 6, "quantity": 1},
-            {"item": "telescope","value": 12, "quantity": 1},
-            {"item": "keyboard", "value": 7, "quantity": 3}])
+            [{"item": "stapler", "value": 1.2},
+            {"item": "teapot", "value": 4},
+            {"item": "magic beans", "value": 6},
+            {"item": "telescope","value": 12},
+            {"item": "keyboard", "value": 7}])
 
 michael = Vendor("Michael", f"Michael: Welcome to my table{player.username}! My items are big - THAT'S WHAT SHE SAID!", 0.6, 
-                [{"item": "coffee mug","value": 0.8,"quantity": 1},
-                {"item": "reem of paper", "value": 0.5,"quantity": 10},
-                {"item": "an award","value": 2,"quantity": 3},
-                {"item": "cented candle","value": 3,"quantity": 2},
-                {"item": "plasma tv","value": 9,"quantity": 1}
+                [{"item": "coffee mug","value": 0.8},
+                {"item": "reem of paper", "value": 0.5},
+                {"item": "an award","value": 2},
+                {"item": "cented candle","value": 3},
+                {"item": "plasma tv","value": 9}
                 ])
 
 kevin = Vendor("Kevin", "Kevin: Hi... I'm Kevin. This is items. We trade.", 0.7, 
-                [{"item": "pot of chili","value": 6,"quantity": 1},
-                {"item": "drum kit","value": 13,"quantity": 1},
-                {"item": "cookie","value": 0.8,"quantity":10},
-                {"item": "calculator","value": 3,"quantity":2},
-                {"item": "ice cream cone","value": 1,"quantity": 4}
+                [{"item": "pot of chili","value": 6},
+                {"item": "drum kit","value": 13},
+                {"item": "cookie","value": 0.8},
+                {"item": "calculator","value": 3},
+                {"item": "ice cream cone","value": 1}
                 ])
 
 angela = Vendor("Angela", "Angela: Good afternoon. Before you I lay several quite valuble objects.", 1, 
-                [{"item": "wood cross","value": 5,"quantity": 1},
-                {"item": "baby jesus statue","value": 7,"quantity": 1},
-                {"item": "open toe shoes","value": 3,"quantity": 3},
-                {"item": "bible","value": 3,"quantity": 2},
-                {"item": "used cat toy","value": 1,"quantity": 4}
+                [{"item": "wood cross","value": 5},
+                {"item": "baby jesus statue","value": 7},
+                {"item": "open toe shoes","value": 3},
+                {"item": "bible","value": 3},
+                {"item": "used cat toy","value": 1}
                 ])
 
 phyllis = Vendor("Phyllis", "Phyllis: Oh, hello! Please, come take a look. Have you met my husband, Bob Vance, Vance Refrigiration?", 0.8,
-                [{"item": "used mini fridge","value": 11,"quantity": 1},
-                {"item": "oven mitt","value": 4,"quantity": 4},
-                {"item": "santa costume","value": 10,"quantity": 1},
-                {"item": "ice cream cake","value": 3,"quantity": 3},
-                {"item": "luxury make up kit","value": 11,"quantity": 1}
+                [{"item": "used mini fridge","value": 11},
+                {"item": "oven mitt","value": 4},
+                {"item": "santa costume","value": 10},
+                {"item": "ice cream cake","value": 3},
+                {"item": "luxury make up kit","value": 11}
                 ])
 
                 
