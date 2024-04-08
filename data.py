0e5b49e1c834936bfb43ee9fccdbc17d07a3c30d
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-04-08 09:32:09 +0000

    correct value of items in data.py

--- data.py
+++ data.py
@@ -24,42 +24,42 @@ class Vendor:
 
 #Vendor objects
 jim = Vendor("Jim", "Jim: Hi there, want to trade something? Take whatever you want!", 0.7, 
-            [{"item": "stapler", "value": 1.2},
-            {"item": "teapot", "value": 4},
-            {"item": "magic beans", "value": 6},
-            {"item": "telescope","value": 12},
-            {"item": "keyboard", "value": 7}])
+            [{"item": "stapler", "value": 1.3},
+            {"item": "teapot", "value": 3},
+            {"item": "magic beans", "value": 4.9},
+            {"item": "telescope","value": 10},
+            {"item": "keyboard", "value": 5.4}])
 
 michael = Vendor("Michael", f"Michael: Welcome to my table{player.username}! My items are big - THAT'S WHAT SHE SAID!", 0.6, 
                 [{"item": "coffee mug","value": 0.8},
                 {"item": "reem of paper", "value": 0.5},
-                {"item": "award","value": 2},
-                {"item": "cented candle","value": 3},
-                {"item": "plasma tv","value": 9}
+                {"item": "award","value": 1.5},
+                {"item": "cented candle","value": 2.2},
+                {"item": "plasma tv","value": 7.1}
                 ])
 
 kevin = Vendor("Kevin", "Kevin: Hi... I'm Kevin. This is items. We trade.", 0.7, 
-                [{"item": "pot of chili","value": 6},
-                {"item": "drum kit","value": 13},
-                {"item": "cookie","value": 0.8},
-                {"item": "calculator","value": 3},
-                {"item": "ice cream cone","value": 1}
+                [{"item": "pot of chili","value": 4.1},
+                {"item": "drum kit","value": 9},
+                {"item": "cookie","value": 0.9},
+                {"item": "calculator","value": 2.3},
+                {"item": "ice cream cone","value": 1.1}
                 ])
 
 angela = Vendor("Angela", "Angela: Good afternoon. Before you I lay several quite valuble objects.", 1, 
-                [{"item": "wood cross","value": 5},
-                {"item": "baby jesus statue","value": 7},
-                {"item": "open toe shoes","value": 3},
-                {"item": "bible","value": 3},
+                [{"item": "wood cross","value": 3.7},
+                {"item": "baby jesus statue","value": 5.9},
+                {"item": "open toe shoes","value": 2.9},
+                {"item": "bible","value": 3.4},
                 {"item": "used cat toy","value": 1}
                 ])
 
 phyllis = Vendor("Phyllis", "Phyllis: Oh, hello! Please, come take a look. Have you met my husband, Bob Vance, Vance Refrigiration?", 0.8,
-                [{"item": "used mini fridge","value": 11},
-                {"item": "oven mitt","value": 4},
-                {"item": "santa costume","value": 10},
-                {"item": "ice cream cake","value": 3},
-                {"item": "luxury make up kit","value": 11}
+                [{"item": "used mini fridge","value": 8.2},
+                {"item": "oven mitt","value": 2.5},
+                {"item": "santa costume","value": 7.8},
+                {"item": "ice cream cake","value": 1.9},
+                {"item": "luxury make up kit","value": 6.5}
                 ])
 
                 
