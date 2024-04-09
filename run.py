de48baa5a60cc505137570f0d0c4190e764d80d5
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-04-09 06:54:10 +0000

    fix blanklines in run.py found in linter

--- run.py
+++ run.py
@@ -1,44 +1,47 @@
 from data import *
 from colorama import Fore, Back, Style, init
-init(autoreset = True)
+init(autoreset=True)
+
 
 def display_instructions():
     """
     Displays title and instructions to the game
     """
-    print(Fore.GREEN + "WELCOME TO THE GARAGE SALE\n") 
+    print(Fore.GREEN + "WELCOME TO THE GARAGE SALE\n")
     print("The game is simple: trade your thumbtack with higher valued items"
-    " at one of the five vendors.\n"
-    "Your score is the amount of cash your inventory is worth when leaving"
-    " the market. If you are really good you reach the most valuble item"
-    " - the telescope.\n"
-    "Remember: the trade deal needs to be good for both you and the vendor -"
-    " or they will deny the deal.\n")
-    
+          " at one of the five vendors.\n"
+          "Your score is the amount of cash your inventory is worth when"
+          " leaving the market. If you are really good you reach the most"
+          " valuble item - the telescope.\n"
+          "Remember: the trade deal needs to be good for both you and"
+          " the vendor - or they will deny the deal.\n")
+
+
 def input_username():
     """
     Lets the user input a username
     """
-    while(True):
+    while (True):
         player.username = input("Enter username here:\n")
         if not player.username.strip():
             print(Fore.RED + "\nPlease enter a username")
         else:
             print(Fore.GREEN + "\nIt is time to start the game."
-            " Let the great experiment begin!\n")
+                               " Let the great experiment begin!\n")
             break
-    
+
+
 def game_menu():
     """
     Displays a menu to allow the user to choose what to do in the game
     """
     choice = ""
-    while(True):
+    while (True):
         print(Fore.MAGENTA + "GAME MENU\n")
         print("1: Go to the garage sale\n"
-        "2: Show your inventory\n" 
-        "3: Show score and exit game\n"
-        "4: Learn more about the vendors")
+              "2: Show your inventory\n"
+              "3: Show score and exit game\n"
+              "4: Learn more about the vendors")
         selected_answer = input("\nChoose an option:\n")
         match selected_answer:
             case "1":
@@ -51,21 +54,22 @@ def game_menu():
                 show_vendor_info()
             case _:
                 print(Fore.RED + "Something went wrong, select a number"
-                "between 1-4\n")
-    
+                                 "between 1-4\n")
+
+
 def go_to_market():
     """
     Allows user to choose which vendor to meet
     """
     choice = ""
-    while(True):
+    while (True):
         print(Fore.GREEN + "\nWelcome to the garage sale! Choose a"
-        " table to visit.\n")
+                           " table to visit.\n")
         print("1: Jim\n"
-        "2: Michael\n" 
-        "3: Angela\n"
-        "4: Kevin\n"
-        "5: Phyllis")
+              "2: Michael\n"
+              "3: Angela\n"
+              "4: Kevin\n"
+              "5: Phyllis")
         selected_answer = input("\nChoose an option:\n")
         match selected_answer:
             case "1":
@@ -85,8 +89,9 @@ def go_to_market():
                 break
             case _:
                 print(Fore.RED + "Something went wrong, select a number "
-                "between 1-5\n")
+                                 "between 1-5\n")
     visit_vendor(choice)
+
 
 def visit_vendor(vendor):
     """
@@ -96,20 +101,21 @@ def visit_vendor(vendor):
     items_list = vendor.items
     for item in items_list:
         print(f"{(items_list.index(item)+1)}: {item["item"].capitalize()}")
-    while(True):
+    while (True):
         try:
             chosen_number = int(input("What would you like to trade? "
-            f"(1-{len(items_list)}):\n"))
+                                      f"(1-{len(items_list)}):\n"))
             if chosen_number > 0 and chosen_number <= len(items_list):
                 i = chosen_number - 1
                 chosen_item = items_list[i]
                 trade(chosen_item, vendor)
                 break
-            else: 
+            else:
                 raise ValueError
         except ValueError:
             print(Fore.RED + f"Something went wrong, select a number between "
-                f"1-{len(items_list)}")
+                             f"1-{len(items_list)}")
+
 
 def trade(item, vendor):
     """
@@ -122,15 +128,16 @@ def trade(item, vendor):
         new_user_item = item
         new_vendor_item = player.items
         print(Fore.GREEN + f"\n{vendor.name}: I will accept that deal."
-         " Here you go.\n")
+                           " Here you go.\n")
         print("-- Trade went through, inventory updated --\n")
         player.items = new_user_item
         player.trades += 1
         vendor.items.append(new_vendor_item)
-        vendor.items.remove(item)   
+        vendor.items.remove(item)
     else:
         print(Fore.CYAN + f"\n{vendor.name}: That's not a good trade for me. "
-        f"Your {player.items["item"]} is not worth enough.\n")
+                          f"Your {player.items["item"]} is not worth"
+                          " enough.\n")
         print("-- Trade failed --\n")
     game_menu()
 
@@ -143,24 +150,25 @@ def show_vendor_info():
     print(
         "Jim - A tall, skinny guy who is always happy."
         " Would love to trade with you! Most valuble item is the telescope.\n"
-        "Michael - A man in a ill fitting suit. Not the best negotiator." 
+        "Michael - A man in a ill fitting suit. Not the best negotiator."
         " Most valuble item is the plasma tv.\n"
         "Angela - A tiny, tiny woman with a stylish cross around her neck."
-        " Doesn't seem to care for you. Most valuble item is the baby jesus" 
+        " Doesn't seem to care for you. Most valuble item is the baby jesus"
         " statue \n"
         "Kevin - A big, bald guy. His items seem to consist of mostly"
         " food and music related stuff. Most valuble item is the drum kit.\n"
         "Phyllis - Kind of looks like your grandma. Her husband seems"
         " to be with her aswell. Most valuble item is the used mini fridge.\n")
 
+
 def display_score_and_exit():
     """
     Displays score and exits game
     """
     print(Fore.GREEN + "\nSCORE\n")
-    print(f"In {player.trades} trades you have reached a value of" 
-    f" ${player.items["value"]},"
-    f" good job {player.username}!")
+    print(f"In {player.trades} trades you have reached a value of"
+          f" ${player.items["value"]},"
+          f" good job {player.username}!")
     while (True):
         answer = input("Are you ready to exit game? (Y/N):\n").lower()
         if answer == "y":
@@ -169,12 +177,13 @@ def display_score_and_exit():
             break
         elif answer == "n":
             print(Fore.GREEN + "Let's go back to the menu"
-            " and trade some more!\n")
+                               " and trade some more!\n")
             game_menu()
             break
         else:
             print(Fore.RED + "Something went wrong, please input"
-            " the letter 'Y' or 'N'\n")
+                             " the letter 'Y' or 'N'\n")
+
 
 def main():
     """
@@ -184,10 +193,6 @@ def main():
     input_username()
     game_menu()
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     main()
-
-
-
-
-
